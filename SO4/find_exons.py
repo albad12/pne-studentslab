25a0b2834435bcd93f153575ddc34ad11cbae36b
albad12 <a.diazp.2025@alumnos.urjc.es>
2026-02-17 16:42:36 +0100

    17/02/2026

--- SO4/find_exons.py
+++ SO4/find_exons.py
@@ -1,3 +1,16 @@
 from pathlib import Path
 
-str = "Sequences/"
+file = "Sequences/ADA_sequence.fa"
+content_1 = Path(file).read_text()
+content_1 = content_1.split("\n")
+content_1 = content_1[1::]
+content_1_new = "".join(content_1)
+
+file2 = "Sequences/ADA_EXONS.txt"
+content_2 = Path(file2).read_text()
+content_2 = content_2.split("\n")
+content_2 = content_2[1::]
+content_2_new = "".join(content_2)
+
+
+
