cc85e18bee078727c967cbd1f10e1d5d5e81aafc
albad12 <a.diazp.2025@alumnos.urjc.es>
2026-02-18 18:44:13 +0100

    18/02/2026

---
+++ P00/Seq0.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+def seq_ping():
+    print("Ok")
+
+def seq_read_fasta(filename):
+    content = Path(filename).read_text()
+    content_new = content.split("\n")
+    result = ("".join(content_new[1::]))
+    return result
+
+def seq_len(seq):
+    result = len(seq)
+    return result
+
+def seq_count_base(seq, base):
+    count = 0
+    for a in seq:
+        if a == base:
+            count += 1
+    return count
+
+def seq_count(seq):
+    d = {"A": 0, "T":0, "G": 0, "C": 0}
+    d["A"] = seq_count_base(seq,"A")
+    d["T"] = seq_count_base(seq,"T")
+    d["G"] = seq_count_base(seq, "G")
+    d["C"] = seq_count_base(seq,"C")
+    return d
+
+def seq_reverse(seq, n):
+    lst = []
+    seq_r = seq[::-1]
+    for base in seq_r:
+        if len(lst) < n :
+            lst.append(base)
+
+    new_seq = ''.join(lst)
+    return new_seq
+
+
+
+
