ea67dc4d0454adc356f584f42fa31aa584570d04
albad12 <a.diazp.2025@alumnos.urjc.es>
2026-02-24 15:29:37 +0100

    24/02/2026

---
+++ S06/test-01.py
@@ -0,0 +1,21 @@
+class Seq:
+    """A class for representing sequences"""
+    pass
+
+# Create am object of the class Seq => We use the name of the class + ()
+s1 = Seq()
+# Create another object of the class Seq
+s2 = Seq()
+
+# The methods are actions that the object can perform
+# The attributes are the data or characteristics that belong to a class or an object
+
+class ClassName:
+    #Constructor (__init__) method to initialize attributes
+def __init__(self, attribute1, attribute2):
+    self.attribute1 = attribute1     # Define instance attribute
+    self.attribute2 = attribute2
+    # Method to define actions or behaviours:
+def method_name(self, parameter):
+    # Perform operations on attributes or other tasks. Code
+    pass
