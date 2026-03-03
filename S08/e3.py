73c3c6f369e50f2583a2580e521abda026d644f8
albad12 <a.diazp.2025@alumnos.urjc.es>
2026-03-03 16:40:19 +0100

    25/02/2026

---
+++ S08/e3.py
@@ -0,0 +1,18 @@
+import socket
+
+
+PORT = 8081
+IP = "212.128.255.74"
+
+while True:
+# -- Ask the user for the message
+    message = str.encode(input("Enter the message: "))
+# -- Create the socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# -- Establish the connection to the Server
+    s.connect((IP, PORT))
+# -- Send the user message
+    s.send(message)
+# -- Close the socket
+    s.close()
+
