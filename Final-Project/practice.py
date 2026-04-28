0c6fb7b0c59f80793a33fe8a05cc6fbba942f71d
albad12 <a.diazp.2025@alumnos.urjc.es>
2026-04-28 16:43:37 +0200

    28/04/2026

--- Final-Project/practice.py
+++ Final-Project/practice.py
@@ -1,9 +1,21 @@
 import http. client
 import json
-specie = input(str("Enter the species name: "))
+
+genes = {"FRAT1": "ENSG00000165879",
+         "ADA": "ENSG00000196839",
+         "FXN": "ENSG00000165060",
+         "RNU6_269P": "ENSG00000212379",
+         "MIR633": "ENSG00000207552",
+         "TTTY4C": "ENSG00000228296",
+         "RBMY2YP": "ENSG00000227633",
+         "FGFR3": "ENSG00000068078",
+         "KDR": "ENSG00000128052",
+         "ANK2": "ENSG00000145362"
+         }
+
 SERVER = 'rest.ensembl.org'
-ENDPOINT = '/info/'
-PARAMS = f"assembly/{specie}?content-type=application/json"
+ENDPOINT = '/sequence/id/'
+PARAMS = f"{genes["MIR633"]}?content-type=application/json"
 URL = SERVER + ENDPOINT + PARAMS
 
 print()
@@ -15,5 +27,28 @@ conn.request("GET", ENDPOINT + PARAMS)
 response = conn.getresponse()
 data = json.loads(response.read().decode())
 print(f"Response received!: {response.status} {response.reason}\n")
-print(data["karyotype"])
+
+
+print(data['id'])
+print(data['seq'])
+seq = data['seq']
+print(f"Total lenght: {len(seq)}")
+def info(seq):
+    print(seq[0])
+    print(seq[-1])
+    print(len(seq))
+    print(data["id"])
+
+def count(seq, base):
+    count = 0
+    for a in seq:
+        if a == base:
+            count += 1
+    return f"{base}: {count} ({round((count / len(seq)) * 100, 1)})%"
+
+
+print(f"{count(seq, "A")}\n"
+        f"{count(seq, "C")}\n"
+        f"{count(seq, "G")}\n"
+        f"{count(seq, "T")}")
 
--- Final-Project/server.py
+++ Final-Project/server.py
@@ -42,18 +42,15 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             SERVER = 'rest.ensembl.org'
             ENDPOINT = '/info/'
             PARAMS = f"assembly/{specie}?content-type=application/json"
-            URL = SERVER + ENDPOINT + PARAMS
-
-            print()
-            print(f"Server: {SERVER}")
-            print(f"URL: {URL}")
 
             conn = http.client.HTTPSConnection(SERVER)
             conn.request("GET", ENDPOINT + PARAMS)
             response = conn.getresponse()
             data = json.loads(response.read().decode())
             print(f"Response received!: {response.status} {response.reason}\n")
-            print(data["karyotype"])
+            kar = data["karyotype"]
+            for i in kar:
+                print(i)
         elif path == "/chromosomeLength":
             pass
         else:
